2e21a05362e82a6ce948da90e25e5b4eb318a52e
Petteri Karsisto <p.karsisto+git@gmail.com>
2021-05-18 18:55:28 +0300

    Include CSS and footer to Pandoc conversion command

--- script/convert-md.sh
+++ script/convert-md.sh
@@ -1,2 +1,4 @@
 # Convert markdown to real page
-pandoc ../markdown/$1.md -f markdown -t html5 -s -o ../html/$1.html
+pandoc ../markdown/$1.md -f markdown -t html5 -s -o ../html/$1.html \
+       -c "https://cdn.simplecss.org/simple.min.css" -c "../css/style.css" \
+       -A "footer.txt"
